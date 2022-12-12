e46f235d4a20a3b775c5278015c9314ff38cf8fe
SeongHo-C <leeseong010@naver.com>
2022-12-12 10:04:56 +0900

    feat(메인): 검색 기능 구현

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import styles from './search.module.css';
 import { BiSearch } from 'react-icons/bi';
 import { BsFillCaretDownFill } from 'react-icons/bs';
+import { projectsContext } from '../../context/projectsContext';
 
 export default function Search() {
   const [type, setType] = useState('제목');
   const [open, setOpen] = useState(false);
+  const { getProjects } = useContext(projectsContext);
+
+  const inputRef = useRef();
 
   const handleChandeTag = (e) => {
     setType(e.target.innerText);
     setOpen(!open);
+  };
+
+  const onSearch = () => {
+    const keyword = inputRef.current.value;
+    const typeChange = type === '제목' ? 'TITLE' : 'TAG';
+
+    getProjects(keyword, typeChange);
   };
 
   return (
@@ -20,12 +31,16 @@ export default function Search() {
           <BsFillCaretDownFill className={styles.selectBtn} />
         </section>
         <input
+          ref={inputRef}
           placeholder='검색을 통해 빠르게 찾아보세요'
           type='text'
           className={styles.text}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') onSearch();
+          }}
         />
       </div>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={onSearch}>
         <BiSearch />
       </button>
 
